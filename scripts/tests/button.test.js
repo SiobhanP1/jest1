347961984124cd6f7277afa2ce99a25c7ae17cbd
SiobhanP1 <siobhanphillips11@gmail.com>
2023-06-28 16:46:41 +0000

    Add test to check that HTML is being opened and read by checking that the h1 exists.

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -3,12 +3,19 @@ const buttonClick = require("../button");
 const changePContent = require("../button");
 
 beforeEach(()=> {
-    document.body.innerHTML = <p id="par"></p>
-})
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
 
 describe("DOM Test", ()=> {
     test("expect p content to change", ()=> {
         changePContent();
-        expect(document.getElementById("par").innerHTML.toEqual("You clicked."));
-    })
+        expect(document.getElementById("par").innerHTML).toEqual("You clicked.");
+    });
+    test("h1 should exist", ()=> {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
 })
